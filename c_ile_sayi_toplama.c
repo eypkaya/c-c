d0f82980637ab777b4893b439870059d67406a33
eypkaya <kaya01266@gmail.com>
2024-12-06 06:55:23 +0300

    sayı toplama yapıldı

---
+++ c_ile_sayi_toplama.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int a, b, toplam;
+printf("İlk sayıyı girin: ");
+scanf("%d", &a);
+printf("İkinci sayıyı girin: ");
+scanf("%d", &b);
+toplam = a + b;
+printf("Toplam: %d\n", toplam);
+return 0;
+}
